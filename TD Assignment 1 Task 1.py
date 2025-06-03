b29ba93c7263d30967c9ba9d763147400fe4c834
Shashidhar Biradar <shashidharbiradar6@gmail.com>
2025-06-03 15:49:46 +0530

    Add files via upload

---
+++ TD Assignment 1 Task 1.py
@@ -0,0 +1,20 @@
+#Task 1: Perform Basic Mathematical Operations
+
+n1=input("Enter first number : ")
+n2=input("Enter second number : ")
+n1=int(n1)
+n2=int(n2)
+
+#addition Operation
+add_ans=n1+n2
+
+#subtraction Operation
+sub_ans=n1-n2
+
+#multiplication Operation
+mul_ans=n1*n2
+
+#division Operation
+div_ans=n1/n2
+
+print("Addition : ",add_ans,"\nSubtraction : ",sub_ans,"\nMultiplication : ",mul_ans,"\nDivision : ",div_ans)
---
+++ TD Assignment 1 Task 2.py
@@ -0,0 +1,11 @@
+#Task 2: Create a Personalized Greeting
+from traceback import print_tb
+
+fname=input("Enter your first name : ")
+lname=input("Enter your last name : ")
+
+fname=str(fname)
+lname=str(lname)
+name=fname+lname
+
+print("Hello, ",name,"! Welcome to the Python Programming.")
